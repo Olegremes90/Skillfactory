0a4da2d3228f6f0087c0a40287b6a08b724a1597
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2022-09-24 16:30:10 +0300

    завершение игры крестики нолики

--- крестики нолики2.py
+++ крестики нолики2.py
@@ -1,3 +1,13 @@
+def greet():
+    print("-------------------")
+    print("  Приветсвуем вас  ")
+    print("      в игре       ")
+    print("  крестики-нолики  ")
+    print("-------------------")
+    print(" формат ввода: x y ")
+    print(" x - номер строки  ")
+    print(" y - номер столбца ")
+
 
 def show():
     print()
@@ -8,25 +18,33 @@ def show():
         print(row_str)
         print("  --------------- ")
     print()
+
+
 def ask():
     while True:
         cords = input("         Ваш ход: ").split()
 
         if len(cords) != 2:
-            print(" Введите координаты: ")
+            print(" Введите 2 координаты! ")
             continue
-        x,y = cords
-        if not(x.isdigit()) or not(y.isdigit()):
-            print('Введите числа:')
+
+        x, y = cords
+
+        if not (x.isdigit()) or not (y.isdigit()):
+            print(" Введите числа! ")
             continue
-        x,y=int(x), int(y)
-        if 0 > x or x > 2 or 0 >y or y > 2:
-            print('Координаты не входят в диапозон')
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Координаты вне диапазона! ")
             continue
-        if field[x][y] !=' ':
-            print('Клетка занята!')
+
+        if field[x][y] != " ":
+            print(" Клетка занята! ")
             continue
-        return x,y
+
+        return x, y
 
 
 def check_win():
@@ -38,14 +56,16 @@ def check_win():
         for c in cord:
             symbols.append(field[c[0]][c[1]])
         if symbols == ["X", "X", "X"]:
-            print("Выиграл ")
+            print("Выиграл X!!!")
             return True
         if symbols == ["0", "0", "0"]:
-            print("Выиграл 0")
+            print("Выиграл 0!!!")
             return True
     return False
 
-field = [[' '] * 3 for i in range(3)]
+
+greet()
+field = [[" "] * 3 for i in range(3)]
 count = 0
 while True:
     count += 1
@@ -68,10 +88,3 @@ while True:
     if count == 9:
         print(" Ничья!")
         break
-
-
-
-
-
-
-
